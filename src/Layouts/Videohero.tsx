606b139ad76d6131c5daf8a531ffee912a39d293
Benjamin Olufemi <benjaminolufemi16@gmail.com>
2025-12-30 12:12:22 -1100

    WIP videohero

--- src/Layouts/Videohero.tsx
+++ src/Layouts/Videohero.tsx
@@ -1,16 +1,16 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useRef } from "react";
 // import brandingvideo from "../assets/brandingvideo.mp4";
-import website from "../assets/website.mp4";
+import website1 from "../assets/website1.mp4";
 
 export default function VideoHero() {
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.playbackRate = 0.55;
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (videoRef.current) {
+  //     videoRef.current.playbackRate = 0.5;
+  //   }
+  // }, []);
 
   return (
     <div className="relative min-h-screen overflow-hidden">
@@ -24,11 +24,11 @@ export default function VideoHero() {
           loop
           playsInline
         >
-          <source src={website} type="video/mp4" />
+          <source src={website1} type="video/mp4" />
         </video>
       </div>
       {/* Dark overlay for better text readability */}
-      <div className="absolute inset-0 bg-gradient-to-t from-gray-300 via-transparent to-transparent z-10"></div>
+      <div className="absolute inset-0 bg-gradient-to-t from-gray-50 via-transparent to-transparent z-10"></div>
 
       {/* Content */}
       <div className="relative z-20 flex flex-col justify-end min-h-screen pb-8 md:pb-16 lg:pb-24 px-8 md:px-16 lg:px-24">
@@ -43,14 +43,14 @@ export default function VideoHero() {
             recharge and scale.
           </p>
 
-          <div className="flex flex-col sm:flex-row gap-4">
+          {/* <div className="flex flex-col sm:flex-row gap-4">
             <button className="bg-[#FED65E] text-[#4C12BF] hover:bg-[#4C12BF] text-[20px] hover:text-[#FED65E] px-10 py-2 rounded-xl cursor-pointer transition-colors duration-200 shadow-xl font-Achivo">
               {"Let's Talk"}
             </button>
             <button className="border-2 border-[#FED65E] text-[#FED65E] hover:bg-[#FED65E] text-[20px] hover:text-[#4C12BF] hover:border-[#4C12BF] bg-[#4C12BF] bg-opacity-80 px-4 py-2 rounded-xl hover:bg-opacity-100 transition-all duration-200 cursor-pointer shadow-xl font-Achivo">
               See our work
             </button>
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
